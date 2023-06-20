aaf428dd9cd6b18041a232069e48542d0eabef03
devopssii <133768745+devopssii@users.noreply.github.com>
2023-06-20 15:26:34 +0500

    Create appapand.py

---
+++ appapand.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+from pandasai import PandasAI
+from pandasai.llm import OpenAI
+import os
+
+# read excel files from directory
+excel_dir = "data/"
+excel_files = [os.path.join(excel_dir, f) for f in os.listdir(excel_dir) if f.endswith(".xlsx")]
+
+# create DataFrame from excel files
+df = pd.concat([pd.read_excel(f) for f in excel_files])
+
+# create OpenAI LLM instance
+llm = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+# create PandasAI instance
+pandas_ai = PandasAI(llm=llm)
+
+def handle_userinput(user_question):
+    # use PandasAI to process user question
+    response = pandas_ai.ask(df, prompt=user_question)
+
+    # display response
+    st.write(response)
+
+def main():
+    st.set_page_config(page_title="Chat with multiple Excel files", page_icon=":books:")
+    st.header("Chat with multiple Excel files :books:")
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
+
+if __name__ == '__main__':
+    main()
