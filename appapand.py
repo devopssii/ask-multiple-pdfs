fee41c223d6bccbc7cde6867b3d84e420a3541bf
devopssii <133768745+devopssii@users.noreply.github.com>
2023-06-20 22:51:33 +0500

    Update appapand.py

--- appapand.py
+++ appapand.py
@@ -8,27 +8,34 @@ from pandasai.llm.openai import OpenAI
 from pandasai.llm.starcoder import Starcoder
 from pandasai.llm.open_assistant import OpenAssistant
 from pandasai.llm.google_palm import GooglePalm
+from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+import numpy as np
+
+load_dotenv()
 
 # read excel files from directory
-excel_dir = "data/"
-excel_files = [os.path.join(excel_dir, f) for f in os.listdir(excel_dir) if f.endswith(".xlsx")]
+#excel_dir = "/home/myhome202323/ask-multiple-pdfs/data/"
+#excel_files = [os.path.join(excel_dir, f) for f in os.listdir(excel_dir) if f.endswith(".xlsx")]
 
 # create DataFrame from excel files
-df = pd.concat([pd.read_excel(f) for f in excel_files])
+#df = pd.concat([pd.read_excel(f) for f in excel_files])
+file_path = '/home/myhome202323/ask-multiple-pdfs/data/krossovki.xlsx'
+df = pd.read_excel(file_path)
 
 # create OpenAI LLM instance
-llm = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
+#llm = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+llm = OpenAI(api_token=os.getenv("OPENAI_API_KEY"), temperature=0)
 # create PandasAI instance
-pandas_ai = PandasAI(llm=llm)
-
+#pandas_ai = PandasAI(llm=llm)
+pandas_ai = PandasAI(llm=llm, conversational=True, save_charts=True, enable_cache=False)
 def handle_userinput(user_question):
     # use PandasAI to process user question
-    response = pandas_ai.ask(df, prompt=user_question)
+    response = pandas_ai.run(df, prompt=user_question)
 
     # display response
     st.write(response)
-
+    st.markdown(response)
 def main():
     st.set_page_config(page_title="Chat with multiple Excel files", page_icon=":books:")
     st.header("Chat with multiple Excel files :books:")
